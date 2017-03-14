5078330583758db1955e37a4a2deeb0d06366a05
Isak Larsson <Isak454@hotmail.com>
2017-03-14 20:05:27 +0100

    Sidebar template

---
+++ js/app.js
@@ -0,0 +1,73 @@
+var app = angular.module('app', ['ngMaterial']);
+
+app.controller('sidenavController', function ($scope, $timeout, $mdSidenav) {
+    var imagePath = 'https://material.angularjs.org/latest/img/list/60.jpeg';
+    $scope.messages = [
+        {
+            profileImg : imagePath,
+            from: 'Min Li Chan',
+            time: '13:36',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+            profileImg : imagePath,
+            from: 'Min Li Chan',
+            time: '13:36',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+            profileImg : imagePath,
+            from: 'Min Li Chan',
+            time: '13:36',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+            profileImg : imagePath,
+            from: 'Min Li Chan',
+            time: '13:36',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+            profileImg : imagePath,
+            from: 'Min Li Chan',
+            time: '13:36',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+            profileImg : imagePath,
+            from: 'Min Li Chan',
+            time: '13:36',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+            profileImg : imagePath,
+            from: 'Min Li Chan',
+            time: '13:36',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+            profileImg : imagePath,
+            from: 'Min Li Chan',
+            time: '13:36',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+            profileImg : imagePath,
+            from: 'Min Li Chan',
+            time: '13:36',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+            profileImg : imagePath,
+            from: 'Min Li Chan',
+            time: '13:36',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+            profileImg : imagePath,
+            from: 'Min Li Chan',
+            time: '13:36',
+            notes: " I'll be in your neighborhood doing errands"
+        }
+    ];
+    });
