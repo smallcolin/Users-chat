bf1d75eaf7d1a87475a8a57a5636c6b82e0fe1bc
Isak Larsson <Isak454@hotmail.com>
2017-03-14 23:25:37 +0100

    Sidenav template done

--- js/app.js
+++ js/app.js
@@ -1,73 +1,146 @@
 var app = angular.module('app', ['ngMaterial']);
 
 app.controller('sidenavController', function ($scope, $timeout, $mdSidenav) {
+
     var imagePath = 'https://material.angularjs.org/latest/img/list/60.jpeg';
     $scope.messages = [
         {
             profileImg : imagePath,
-            from: 'Min Li Chan',
+            from: "Sebastian Thorsen",
             time: '13:36',
-            notes: " I'll be in your neighborhood doing errands"
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
         },
         {
             profileImg : imagePath,
-            from: 'Min Li Chan',
+            from: "Sebastian Thorsen",
             time: '13:36',
-            notes: " I'll be in your neighborhood doing errands"
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
         },
         {
             profileImg : imagePath,
-            from: 'Min Li Chan',
+            from: "Sebastian Thorsen",
             time: '13:36',
-            notes: " I'll be in your neighborhood doing errands"
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
         },
         {
             profileImg : imagePath,
-            from: 'Min Li Chan',
+            from: "Sebastian Thorsen",
             time: '13:36',
-            notes: " I'll be in your neighborhood doing errands"
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
         },
         {
             profileImg : imagePath,
-            from: 'Min Li Chan',
+            from: "Sebastian Thorsen",
             time: '13:36',
-            notes: " I'll be in your neighborhood doing errands"
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
         },
         {
             profileImg : imagePath,
-            from: 'Min Li Chan',
+            from: "Sebastian Thorsen",
             time: '13:36',
-            notes: " I'll be in your neighborhood doing errands"
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
         },
         {
             profileImg : imagePath,
-            from: 'Min Li Chan',
+            from: "Sebastian Thorsen",
             time: '13:36',
-            notes: " I'll be in your neighborhood doing errands"
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
         },
         {
             profileImg : imagePath,
-            from: 'Min Li Chan',
+            from: "Sebastian Thorsen",
             time: '13:36',
-            notes: " I'll be in your neighborhood doing errands"
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
         },
         {
             profileImg : imagePath,
-            from: 'Min Li Chan',
+            from: "Sebastian Thorsen",
             time: '13:36',
-            notes: " I'll be in your neighborhood doing errands"
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
         },
         {
             profileImg : imagePath,
-            from: 'Min Li Chan',
+            from: "Sebastian Thorsen",
             time: '13:36',
-            notes: " I'll be in your neighborhood doing errands"
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
         },
         {
             profileImg : imagePath,
-            from: 'Min Li Chan',
+            from: "Sebastian Thorsen",
             time: '13:36',
-            notes: " I'll be in your neighborhood doing errands"
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
+        },
+        {
+            profileImg : imagePath,
+            from: "Sebastian Thorsen",
+            time: '13:36',
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
+        },
+        {
+            profileImg : imagePath,
+            from: "Sebastian Thorsen",
+            time: '13:36',
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
+        },
+        {
+            profileImg : imagePath,
+            from: "Sebastian Thorsen",
+            time: '13:36',
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
+        },
+        {
+            profileImg : imagePath,
+            from: "Sebastian Thorsen",
+            time: '13:36',
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
+        },
+        {
+            profileImg : imagePath,
+            from: "Sebastian Thorsen",
+            time: '13:36',
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
+        },
+        {
+            profileImg : imagePath,
+            from: "Sebastian Thorsen",
+            time: '13:36',
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
+        },
+        {
+            profileImg : imagePath,
+            from: "Sebastian Thorsen",
+            time: '13:36',
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
+        },
+        {
+            profileImg : imagePath,
+            from: "Sebastian Thorsen",
+            time: '13:36',
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
+        },
+        {
+            profileImg : imagePath,
+            from: "Sebastian Thorsen",
+            time: '13:36',
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
+        },
+        {
+            profileImg : imagePath,
+            from: "Sebastian Thorsen",
+            time: '13:36',
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
+        },
+        {
+            profileImg : imagePath,
+            from: "Sebastian Thorsen",
+            time: '13:36',
+            notes: "Weeeew lad, you be fly af duuuuuuuuuuuuude, lmao xd xdxdx "
         }
     ];
+
+    $scope.openMenu = function () {
+        $mdSidenav('left').toggle();
+    }
+
+
     });
