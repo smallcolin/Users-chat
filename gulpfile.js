d62b2fab9d16470647c0a7753261d10e54243b21
Erik Tedfelt Lennström <erik.tedfelt.lennstrom@gmail.com>
2017-03-16 15:26:54 +0100

    Started on gulpfile and fixed folders

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var concat = require('gulp-concat'),
+	sass = rewuire('gulp-sass')
+	gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	ngAnnotate = require('gulp-ng-annotate');
+
+gulp.task('ngAnnotate', function (){
+
+});
