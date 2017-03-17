c88201e1449b317e6f3c3a3cc20b7559e0c6e433
oskar eriksson <ogg_ish@hotmail.com>
2017-03-17 16:45:42 +0100

    First draft section-news

--- assets/js/scripts-oskar.js
+++ assets/js/scripts-oskar.js
@@ -1,6 +1,6 @@
 var app = angular.module('app', [] );
 
-
+/*
 app.controller('chatSection' , function($scope) {
 	$scope.testChat = [
 	{author:'you', image: '../assets/img/you.jpg',status: 'online', message:'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
@@ -10,3 +10,20 @@ app.controller('chatSection' , function($scope) {
 	];
 	console.log($scope.test);
 });	
+*/
+
+
+app.controller('newsSection' , function($scope) {
+
+	$scope.fakeNews = [
+	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy you',time: '16:30',image: '../assets/img/other.jpg'},
+	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy them',time: '16.29',image: '../assets/img/other.jpg'},
+	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy her', time: '16:28',image: '../assets/img/other.jpg'},
+	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy him', time: '16:27',image: '../assets/img/other.jpg'},
+	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy this', time: '16:26',image: '../assets/img/other.jpg'},
+	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy that', time: '16:25',image: '../assets/img/other.jpg'},
+	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy shit', time: '16:24',image: '../assets/img/other.jpg'},
+	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy rofl', time: '16:23',image: '../assets/img/other.jpg'}
+	];
+
+});
