68a2563ebb955ed59c3acb281e310b4ad7f07129
oskar eriksson <ogg_ish@hotmail.com>
2017-03-17 12:46:53 +0100

    First draft chat-window

--- assets/js/scripts-oskar.js
+++ assets/js/scripts-oskar.js
@@ -3,10 +3,10 @@ var app = angular.module('app', [] );
 
 app.controller('chatSection' , function($scope) {
 	$scope.testChat = [
-	{author:'you', image: 'you.jpg', message:'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'other', image: 'other.jpg', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'other', image: 'other.jpg', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: 'you.jpg', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'}
+	{author:'you', image: '../assets/img/you.jpg',status: 'online', message:'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+	{author:'other', image: '../assets/img/other.jpg',status: 'offline', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+	{author:'other', image: '../assets/img/other.jpg',status: 'offline', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'}
 	];
 	console.log($scope.test);
 });	
