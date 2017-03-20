de44dc221ef7f7d598095518d0a484fb20a599af
jakobjohansson <jakobjohansson2@icloud.com>
2017-03-20 21:37:49 +0100

    fix oskars tab indentation......

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,56 +1,55 @@
 var app = angular.module('app', [] );
 
-
 app.controller('chatSection' , function($scope) {
-	$scope.testChat = [
-	{author:'you', image: '../assets/img/you.jpg',status: 'online', message:'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'other', image: '../assets/img/other.jpg',status: 'offline', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'other', image: '../assets/img/other.jpg',status: 'offline', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
-	{author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'}
-	];
+    $scope.testChat = [
+        {author:'you', image: '../assets/img/you.jpg',status: 'online', message:'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'other', image: '../assets/img/other.jpg',status: 'offline', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'other', image: '../assets/img/other.jpg',status: 'offline', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'},
+        {author:'you' , image: '../assets/img/you.jpg',status: 'online', message: 'Lorem ipsum dolor loreoreolerela fakjnfaskfkf'}
+    ];
 });
 
 app.controller('newsSection' , function($scope) {
-	$scope.fakeNews = [
-	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy you',time: '16:30',image: '../assets/img/other.jpg'},
-	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy them',time: '16.29',image: '../assets/img/other.jpg'},
-	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy her', time: '16:28',image: '../assets/img/other.jpg'},
-	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy him', time: '16:27',image: '../assets/img/other.jpg'},
-	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy this', time: '16:26',image: '../assets/img/other.jpg'},
-	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy that', time: '16:25',image: '../assets/img/other.jpg'},
-	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy shit', time: '16:24',image: '../assets/img/other.jpg'},
-	{author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy rofl', time: '16:23',image: '../assets/img/other.jpg'}
-	];
+    $scope.fakeNews = [
+        {author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy you',time: '16:30',image: '../assets/img/other.jpg'},
+        {author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy them',time: '16.29',image: '../assets/img/other.jpg'},
+        {author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy her', time: '16:28',image: '../assets/img/other.jpg'},
+        {author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy him', time: '16:27',image: '../assets/img/other.jpg'},
+        {author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy this', time: '16:26',image: '../assets/img/other.jpg'},
+        {author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy that', time: '16:25',image: '../assets/img/other.jpg'},
+        {author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy shit', time: '16:24',image: '../assets/img/other.jpg'},
+        {author: 'Bill Gates',status: 'online', excerpt: 'Im going to destroy rofl', time: '16:23',image: '../assets/img/other.jpg'}
+    ];
 });
 
 app.directive('autoResize', function() {
-	return {
-		restrict: 'A',
-		scope: true,
-		require: 'ngModel',
-		link: function(scope, element) {
-			scope.$watch('message.content', function(newValue) {
-				if (newValue == null) {
-					return;
-				}
-				if (element[0].scrollHeight > element[0].clientHeight) {
-  					element[0].style.height = element[0].scrollHeight + 5 + "px";
-				}
-			});
-		}
-	}
+    return {
+        restrict: 'A',
+        scope: true,
+        require: 'ngModel',
+        link: function(scope, element) {
+            scope.$watch('message.content', function(newValue) {
+                if (newValue == null) {
+                    return;
+                }
+                if (element[0].scrollHeight > element[0].clientHeight) {
+                    element[0].style.height = element[0].scrollHeight + 5 + "px";
+                }
+            });
+        }
+    }
 });
