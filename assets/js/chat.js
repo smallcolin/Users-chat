7cdcc4176bdc413b6705e02c6ed3bb95ebacf653
jakobjohansson <jakobjohansson2@icloud.com>
2017-03-21 20:55:35 +0100

    autoResize finally working

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -53,24 +53,22 @@ app.controller('newsSection' , function($scope) {
 
 app.directive('autoResize', function($window) {
     return {
-        // restrict: 'A',
-        // require: 'ngModel',
-        // link: function(scope, element, controller) {
-        //     scope.$watch('content.message', function(newValue) {
-        //
-        //         var msg = angular.element("#msg")[0];
-        //         console.log(msg);
-        //         if (newValue == null || newValue == "") {
-        //             msg.style.height = "calc(100% - 181px)";
-        //             element[0].style.height = "40px";
-        //         } else {
-        //             element[0].style.height = 'auto';
-        //             element[0].style.height = element[0].scrollHeight + "px";
-        //             var newHeight = $window.innerHeight - 145 - element[0].clientHeight;
-        //             msg.style.height = newHeight + "px";
-        //         }
-        //     });
-        // }
+        restrict: 'A',
+        link: function(scope, element, controller) {
+            scope.$watch('content.message', function(newValue) {
+                var msg = element[0].children[1];
+                var textarea = element[0].children[2].children[0].children[0].children[0];
+                if (newValue == null || newValue == "") {
+                    msg.style.height = "calc(100% - 181px)";
+                    textarea.style.height = "40px";
+                } else {
+                    textarea.style.height = 'auto';
+                    textarea.style.height = textarea.scrollHeight + "px";
+                    var newHeight = $window.innerHeight - 145 - textarea.clientHeight;
+                    msg.style.height = newHeight + "px";
+                }
+            });
+        }
     }
 });
 
