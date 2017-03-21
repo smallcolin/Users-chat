d74d6054d77c71f8163841b79799e3546c45c47e
Erik Tedfelt Lennström <erik.tedfelt.lennstrom@gmail.com>
2017-03-21 11:36:46 +0100

    Fixe dependecies ui router

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,17 @@
-var app = angular.module('app', [] );
+var app = angular.module('app', ['ui.router'] );
+
+
+app.config(function ($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise("feed");
+
+    $stateProvider
+        .state('feed', {
+            name: 'feed',
+            controller: 'newsSection',
+            templateUrl: 'partials/news-section',
+            url: 'feed'
+        })
+        .state('chat', {
+            name: 'chat'
+        })
+});
