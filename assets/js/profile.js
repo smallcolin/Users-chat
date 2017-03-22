e35929985cf8429a619e61bd1a4013ff1dbf1de4
Erik Tedfelt Lennström <erik.tedfelt.lennstrom@gmail.com>
2017-03-22 15:52:22 +0100

    Add directive to change theme

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,9 +1,30 @@
 
 
-app.controller('settingsProfile' , function ($scope) {
+app.controller('settingsProfile' , function ($scope, $document) {
     $scope.imgSrc = 'assets/img/jobs.png';
     $scope.showName = 'Visningsnamn';
     $scope.userName = 'Username';
     $scope.yourEmail = 'namn.efternamn@exempel.com';
-    $scope.yourInfo = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada, est vitae commodo dapibus, ipsum velit euismod nibh, sit amet efficitur justo est ac augue.'
+    $scope.yourInfo = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada, est vitae commodo dapibus, ipsum velit euismod nibh, sit amet efficitur justo est ac augue.';
+    console.log($scope.lightTheme);
+    $scope.test = function () {
+
+
+    };
+});
+app.directive( 'bodyClass', function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, attrs, element, controller) {
+            scope.$watch('lightTheme', function(newValue){
+                if (newValue == true) {
+                    angular.element('body').addClass('light');
+
+                } else {
+                    angular.element('body').removeClass('light');
+                }
+            });
+        }
+    }
 });
