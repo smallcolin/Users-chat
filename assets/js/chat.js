f05564d2380e71a1d90622c597b513029366ae03
oggzr <ogg_ish@hotmail.com>
2017-03-23 13:13:11 +0100

    Added shift + enter line break in enterSubmit dire

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -91,7 +91,8 @@ app.directive('enterSubmit', function () {
         restrict : 'A',
         link: function (scope, element, attrs) {
             element.bind("keydown keypress", function (event) {
-                if(event.which === 13) {
+
+                if(event.which === 13 && !event.shiftKey) {
                     scope.$apply(function (){
                     scope.$eval(attrs.enterSubmit);
                 });
